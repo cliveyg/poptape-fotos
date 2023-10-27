960f4919f3376c964f44c4a8c50450a11da70706
cliveyg <junkcat@gmail.com>
2023-10-26 21:17:45 -0300

    Fix multiple errors and remove address repo code

--- app/services.py
+++ app/services.py
@@ -3,8 +3,7 @@ import requests
 
 # -----------------------------------------------------------------------------
 
+
 def call_requests(url, headers):
     r = requests.get(url, headers=headers)
     return r
-
-
